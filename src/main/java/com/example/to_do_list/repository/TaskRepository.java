16e46f286822cc486156b513983fbb5cbf7e6c0b
harshaadeshmukh <harshad.deshmukh23@vit.edu>
2026-02-10 16:13:15 +0530

    updated

--- src/main/java/com/example/to_do_list/repository/TaskRepository.java
+++ src/main/java/com/example/to_do_list/repository/TaskRepository.java
@@ -1,8 +1,9 @@
 package com.example.to_do_list.repository;
 
 import com.example.to_do_list.models.Task;
-import org.jetbrains.annotations.NotNull;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<@NotNull Task,@NotNull Long> {
 }
+    
